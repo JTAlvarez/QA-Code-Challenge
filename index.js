1771edf9182488f8954d66ab2cf177506c8223f4
Juan <terodius@gmail.com>
2021-04-30 00:05:41 -0300

    filet test with result

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-let fs = require('fs');
+const fs = require('fs');
+const prompt = require('prompt-sync')();
 
+
+// File reading
 let file1 = fs.readFileSync('File1.txt', 'utf8');
 
+
+// User input
+let userInput = prompt("Enter your sentence : ");
+
 let r = /[,.\s]+/;
-let words = r.test(file1);
+//let words = r.test(file1);
+
+
+let match = r.test(userInput)
 /* let file 1 = fs.read('File1.txt', utf8, function(err, data){
     console.log(file1); 
 }
@@ -11,7 +21,7 @@ let words = r.test(file1);
 
 console.log(file1);
 
-console.log(words);
+console.log(match);
 
 
 //RegExp.prototype.test()
