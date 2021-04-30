d7ae54135e339d4c34aee745b97c2f6dd5e1ef87
Juan <terodius@gmail.com>
2021-04-29 23:44:23 -0300

    file test

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ let fs = require('fs');
 
 let file1 = fs.readFileSync('File1.txt', 'utf8');
 
+let r = /[,.\s]+/;
+let words = r.test(file1);
+/* let file 1 = fs.read('File1.txt', utf8, function(err, data){
+    console.log(file1); 
+}
+*/
+
 console.log(file1);
 
+console.log(words);
 
-// fs.read(fd, [options,] callback)
 
+//RegExp.prototype.test()
 
+//String.prototype.match()
